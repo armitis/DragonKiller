03c5453627ec0ba3d08823a8b85906240a08726f
zhengzhiwen <armitis@163.com>
2024-01-31 01:43:52 +0800

    合并两个有序数组

---
+++ LeetCode/LeetCode/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  LeetCode
+//
+//  Created by 郑智文 on 2024/1/31.
+//
+
+import Foundation
+
+var nums1 = Array(repeating: 0, count: 1)
+var nums2 = [1]
+Solution.merge(&nums1, 0, nums2, 1)
+print(nums1)
+
+
