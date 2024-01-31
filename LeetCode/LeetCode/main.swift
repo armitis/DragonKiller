d6decb85808f0a80c88e82df4102d78ff66d5fbf
zhengzhiwen <armitis@163.com>
2024-02-01 00:49:46 +0800

    原地删除数组重复的数，最多只能有2个重复的数

--- LeetCode/LeetCode/main.swift
+++ LeetCode/LeetCode/main.swift
@@ -7,9 +7,16 @@
 
 import Foundation
 
-var nums1 = Array(repeating: 0, count: 1)
-var nums2 = [1]
-Solution.merge(&nums1, 0, nums2, 1)
-print(nums1)
+//var nums1 = Array(repeating: 0, count: 1)
+//var nums2 = [1]
+//Solution.merge(&nums1, 0, nums2, 1)
+//print(nums1)
 
+var nums = [1]
+let count = Solution.removeDuplicates(&nums)
+var index = 0
+while index < count {
+    print("\(nums[index])")
+    index += 1
+}
 
